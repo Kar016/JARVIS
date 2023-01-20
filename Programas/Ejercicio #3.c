0c5cbf543a58c4c1126d00b9dbdaa440fbbd7f60
Karen Rocely <karenrocely01@gmail.com>
2023-01-20 11:55:05 -0400

    Programas

---
+++ Programas/Ejercicio #3.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main ()
+{
+    char nombre[20];
+    int edad;
+
+    edad = 0;
+
+    printf("Cual es tu nombre?:\n\n");
+    scanf("%s",&nombre);
+
+    printf("Cual es tu edad?:\n\n");
+    scanf("%i",&edad);
+
+    printf("Hola %s Bienvenido al curso.\n",nombre);
+
+    getch ();
+    return 0;
+}
