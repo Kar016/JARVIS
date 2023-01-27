21b64dfa238523fb4399d092ddbb286623892a9c
Karen Rocely <karenrocely01@gmail.com>
2023-01-26 20:55:23 -0400

    Ejercicio #5

---
+++ Programas/Ejercicio #5.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include<conio.h>
+
+/* Programa 1.1
+El siguiente es el primer programa escrito en el lenguaje C. */
+void main (void)
+{
+printf("Mi primer programa en C");
+
+getch ();
+}
