2dcac941668e31611f7e45f4fec4a543f5adab45
Karen Rocely <karenrocely01@gmail.com>
2023-03-10 12:23:55 -0400

    Hey

--- Caracteres/Ejercicio #17.c
+++ Caracteres/Ejercicio #17.c
@@ -1,9 +1,15 @@
 #include<stdio.h>
 #include<string.h>
 
-char * inverso(char *);
+/* Cadena invertida.
+El programa obtiene la cadena invertida. */
+
+char * inverso(char *);   // Funion
+
 
 void main (void)
+
+// La función calcula el inverso de una cadena y regresa el resultado al programa principal.
 {
     char fra[50], aux[50];
 
@@ -22,6 +28,15 @@ char * inverso (char *cadena)
     lon = strlen(cadena);
     j = lon-1;
 
-    while (i)
+    while (i<((lon-1)/2))      // Observa que el reemplazo de los caracteres se debe realizar solamente hasta la mitad de la cadena.
+    {
+        cad = cadena[i];
+        cadena[i] = cadena[j];
+        cadena[j] = cad;
+        i++;
+        j--;
+    }
+
+    return (cadena);
 }
 
