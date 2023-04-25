edd91b770cbb8434f725e9e2bac5bf8259306605
Karen <122666632+Kar016@users.noreply.github.com>
2023-04-25 11:42:18 -0700

    Add files via upload

--- C##/Programa #1/Problema #13/Problema #13/Form1.Designer.cs
+++ C##/Programa #1/Problema #13/Problema #13/Form1.Designer.cs
@@ -29,12 +29,97 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.lblValor = new System.Windows.Forms.Label();
+            this.btnInicio = new System.Windows.Forms.Button();
+            this.btnDetener = new System.Windows.Forms.Button();
+            this.trbIntervalo = new System.Windows.Forms.TrackBar();
+            this.lblTValor = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.trbIntervalo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 500;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // lblValor
+            // 
+            this.lblValor.AutoSize = true;
+            this.lblValor.Location = new System.Drawing.Point(34, 39);
+            this.lblValor.Name = "lblValor";
+            this.lblValor.Size = new System.Drawing.Size(35, 13);
+            this.lblValor.TabIndex = 0;
+            this.lblValor.Text = "label1";
+            // 
+            // btnInicio
+            // 
+            this.btnInicio.Location = new System.Drawing.Point(175, 29);
+            this.btnInicio.Name = "btnInicio";
+            this.btnInicio.Size = new System.Drawing.Size(75, 23);
+            this.btnInicio.TabIndex = 1;
+            this.btnInicio.Text = "Iniciar";
+            this.btnInicio.UseVisualStyleBackColor = true;
+            this.btnInicio.Click += new System.EventHandler(this.btnInicio_Click);
+            // 
+            // btnDetener
+            // 
+            this.btnDetener.Location = new System.Drawing.Point(175, 58);
+            this.btnDetener.Name = "btnDetener";
+            this.btnDetener.Size = new System.Drawing.Size(75, 23);
+            this.btnDetener.TabIndex = 2;
+            this.btnDetener.Text = "Detener";
+            this.btnDetener.UseVisualStyleBackColor = true;
+            this.btnDetener.Click += new System.EventHandler(this.btnDetener_Click);
+            // 
+            // trbIntervalo
+            // 
+            this.trbIntervalo.Location = new System.Drawing.Point(12, 124);
+            this.trbIntervalo.Maximum = 1000;
+            this.trbIntervalo.Minimum = 100;
+            this.trbIntervalo.Name = "trbIntervalo";
+            this.trbIntervalo.Size = new System.Drawing.Size(256, 45);
+            this.trbIntervalo.TabIndex = 3;
+            this.trbIntervalo.TickFrequency = 100;
+            this.trbIntervalo.Value = 100;
+            this.trbIntervalo.Scroll += new System.EventHandler(this.trbIntervalo_Scroll);
+            // 
+            // lblTValor
+            // 
+            this.lblTValor.AutoSize = true;
+            this.lblTValor.Location = new System.Drawing.Point(118, 172);
+            this.lblTValor.Name = "lblTValor";
+            this.lblTValor.Size = new System.Drawing.Size(35, 13);
+            this.lblTValor.TabIndex = 4;
+            this.lblTValor.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(280, 253);
+            this.Controls.Add(this.lblTValor);
+            this.Controls.Add(this.trbIntervalo);
+            this.Controls.Add(this.btnDetener);
+            this.Controls.Add(this.btnInicio);
+            this.Controls.Add(this.lblValor);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            ((System.ComponentModel.ISupportInitialize)(this.trbIntervalo)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label lblValor;
+        private System.Windows.Forms.Button btnInicio;
+        private System.Windows.Forms.Button btnDetener;
+        private System.Windows.Forms.TrackBar trbIntervalo;
+        private System.Windows.Forms.Label lblTValor;
     }
 }
 
--- C##/Programa #1/Problema #13/Problema #13/Form1.cs
+++ C##/Programa #1/Problema #13/Problema #13/Form1.cs
@@ -12,9 +12,43 @@ namespace Problema__13
 {
     public partial class Form1 : Form
     {
+        private int conteo;
+
         public Form1()
         {
             InitializeComponent();
+            conteo = 0;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            conteo++;
+            lblValor.Text = conteo.ToString();
+
+        }
+
+        private void btnInicio_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = true;
+        }
+
+        private void btnDetener_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = false;
+            trbIntervalo.Value = trbIntervalo.Minimum;
+            lblTValor.Text = trbIntervalo.Value.ToString();
+            timer1.Interval = trbIntervalo.Value;
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            timer1.Enabled = false;
+        }
+
+        private void trbIntervalo_Scroll(object sender, EventArgs e)
+        {
+            lblTValor.Text = trbIntervalo.Value.ToString();
+            timer1.Interval = trbIntervalo.Value; // Sirve para cambiar la frecuencia del intervalo
         }
     }
 }
