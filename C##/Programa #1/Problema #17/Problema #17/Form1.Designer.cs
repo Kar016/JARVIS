b8836f92c15a7ae4b6b700443f449db132516f29
Karen <122666632+Kar016@users.noreply.github.com>
2023-04-26 00:52:11 -0700

    Add files via upload

---
+++ C##/Programa #1/Problema #17/Problema #17/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace Problema__17
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dudFrutas = new System.Windows.Forms.DomainUpDown();
+            this.btnAdicionar = new System.Windows.Forms.Button();
+            this.textFruta = new System.Windows.Forms.TextBox();
+            this.lblMensaje = new System.Windows.Forms.Label();
+            this.btnBorrar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // dudFrutas
+            // 
+            this.dudFrutas.Location = new System.Drawing.Point(12, 12);
+            this.dudFrutas.Name = "dudFrutas";
+            this.dudFrutas.Size = new System.Drawing.Size(120, 20);
+            this.dudFrutas.TabIndex = 0;
+            this.dudFrutas.Text = " Frutas";
+            this.dudFrutas.SelectedItemChanged += new System.EventHandler(this.dudFrutas_SelectedItemChanged);
+            // 
+            // btnAdicionar
+            // 
+            this.btnAdicionar.Location = new System.Drawing.Point(158, 98);
+            this.btnAdicionar.Name = "btnAdicionar";
+            this.btnAdicionar.Size = new System.Drawing.Size(75, 23);
+            this.btnAdicionar.TabIndex = 1;
+            this.btnAdicionar.Text = "Adicionar";
+            this.btnAdicionar.UseVisualStyleBackColor = true;
+            this.btnAdicionar.Click += new System.EventHandler(this.btnAdicionar_Click);
+            // 
+            // textFruta
+            // 
+            this.textFruta.Location = new System.Drawing.Point(12, 100);
+            this.textFruta.Name = "textFruta";
+            this.textFruta.Size = new System.Drawing.Size(100, 20);
+            this.textFruta.TabIndex = 2;
+            // 
+            // lblMensaje
+            // 
+            this.lblMensaje.AutoSize = true;
+            this.lblMensaje.Location = new System.Drawing.Point(12, 48);
+            this.lblMensaje.Name = "lblMensaje";
+            this.lblMensaje.Size = new System.Drawing.Size(35, 13);
+            this.lblMensaje.TabIndex = 3;
+            this.lblMensaje.Text = "label1";
+            // 
+            // btnBorrar
+            // 
+            this.btnBorrar.Location = new System.Drawing.Point(158, 127);
+            this.btnBorrar.Name = "btnBorrar";
+            this.btnBorrar.Size = new System.Drawing.Size(75, 23);
+            this.btnBorrar.TabIndex = 4;
+            this.btnBorrar.Text = "Borrar";
+            this.btnBorrar.UseVisualStyleBackColor = true;
+            this.btnBorrar.Click += new System.EventHandler(this.btnBorrar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(291, 287);
+            this.Controls.Add(this.btnBorrar);
+            this.Controls.Add(this.lblMensaje);
+            this.Controls.Add(this.textFruta);
+            this.Controls.Add(this.btnAdicionar);
+            this.Controls.Add(this.dudFrutas);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DomainUpDown dudFrutas;
+        private System.Windows.Forms.Button btnAdicionar;
+        private System.Windows.Forms.TextBox textFruta;
+        private System.Windows.Forms.Label lblMensaje;
+        private System.Windows.Forms.Button btnBorrar;
+    }
+}
+
---
+++ C##/Programa #1/Problema #17/Problema #17/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Problema__17
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dudFrutas.Items.Add("Melon");
+            dudFrutas.Items.Add("Manzana");
+        }
+
+        private void btnAdicionar_Click(object sender, EventArgs e)
+        {
+            dudFrutas.Items.Add(textFruta.Text);
+
+            textFruta.Text = "";
+        }
+
+        private void dudFrutas_SelectedItemChanged(object sender, EventArgs e)
+        {
+            string fruta = (string) dudFrutas.SelectedItem;
+
+            lblMensaje.Text = string.Format("Tu fruta favorita es {0}, es muy sabrosa",fruta);
+        }
+
+        private void btnBorrar_Click(object sender, EventArgs e)
+        {
+            int indice = dudFrutas.SelectedIndex;
+
+            dudFrutas.Items.RemoveAt(indice);
+            dudFrutas.SelectedIndex = 0;
+        }
+    }
+}
