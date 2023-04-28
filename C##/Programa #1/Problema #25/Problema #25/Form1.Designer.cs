93bb4961fe74011980d09bd94e078506370834b5
Karen <122666632+Kar016@users.noreply.github.com>
2023-04-27 18:33:19 -0700

    Add files via upload

---
+++ C##/Programa #1/Problema #25/Problema #25/Form1.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace Problema__25
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.textDato = new System.Windows.Forms.TextBox();
+            this.btnProcesar = new System.Windows.Forms.Button();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textDato
+            // 
+            this.textDato.Location = new System.Drawing.Point(12, 12);
+            this.textDato.Name = "textDato";
+            this.textDato.Size = new System.Drawing.Size(100, 20);
+            this.textDato.TabIndex = 0;
+            this.textDato.TextChanged += new System.EventHandler(this.textDato_TextChanged);
+            // 
+            // btnProcesar
+            // 
+            this.btnProcesar.Location = new System.Drawing.Point(170, 12);
+            this.btnProcesar.Name = "btnProcesar";
+            this.btnProcesar.Size = new System.Drawing.Size(75, 23);
+            this.btnProcesar.TabIndex = 1;
+            this.btnProcesar.Text = "Procesar";
+            this.btnProcesar.UseVisualStyleBackColor = true;
+            this.btnProcesar.Click += new System.EventHandler(this.btnProcesar_Click);
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(265, 225);
+            this.Controls.Add(this.btnProcesar);
+            this.Controls.Add(this.textDato);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textDato;
+        private System.Windows.Forms.Button btnProcesar;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
+    }
+}
+
---
+++ C##/Programa #1/Problema #25/Problema #25/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Problema__25
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnProcesar_Click(object sender, EventArgs e)
+        {
+            bool error = false;
+
+            foreach (char caracter in textDato.Text)  // Desgloza el dato ingresado
+            {
+                if (char.IsDigit(caracter))  // Identifica si en el dato hay un numero
+                {
+                    error = true;  // Marca un error cuando hay un numero
+                    break;
+                }
+            }
+
+            if (error) 
+            {
+                errorProvider1.SetError(textDato, "No se admiten numeros");
+            }
+            else
+            { errorProvider1.Clear(); }
+        }
+
+        private void textDato_TextChanged(object sender, EventArgs e)
+        {
+            bool error = false;
+
+            foreach (char caracter in textDato.Text)  // Desgloza el dato ingresado
+            {
+                if (char.IsDigit(caracter))  // Identifica si en el dato hay un numero
+                {
+                    error = true;  // Marca un error cuando hay un numero
+                    break;
+                }
+            }
+
+            if (error)
+            {
+                errorProvider1.SetError(textDato, "No se admiten numeros");
+            }
+            else
+            { errorProvider1.Clear(); }
+        }
+    }
+}
