0092c47409dfa0fb267ea4d07e4dbd8d9f10b3ca
Karen <122666632+Kar016@users.noreply.github.com>
2023-04-27 18:26:35 -0700

    Add files via upload

---
+++ C##/Programa #1/Problema #23/Problema #23/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace Problema__23
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnOperaciones = new System.Windows.Forms.Button();
+            this.lblFecha = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnOperaciones
+            // 
+            this.btnOperaciones.Location = new System.Drawing.Point(140, 86);
+            this.btnOperaciones.Name = "btnOperaciones";
+            this.btnOperaciones.Size = new System.Drawing.Size(75, 23);
+            this.btnOperaciones.TabIndex = 0;
+            this.btnOperaciones.Text = "Operaciones";
+            this.btnOperaciones.UseVisualStyleBackColor = true;
+            this.btnOperaciones.Click += new System.EventHandler(this.btnOperaciones_Click);
+            // 
+            // lblFecha
+            // 
+            this.lblFecha.AutoSize = true;
+            this.lblFecha.Location = new System.Drawing.Point(161, 38);
+            this.lblFecha.Name = "lblFecha";
+            this.lblFecha.Size = new System.Drawing.Size(0, 13);
+            this.lblFecha.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(380, 268);
+            this.Controls.Add(this.lblFecha);
+            this.Controls.Add(this.btnOperaciones);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnOperaciones;
+        private System.Windows.Forms.Label lblFecha;
+    }
+}
+
