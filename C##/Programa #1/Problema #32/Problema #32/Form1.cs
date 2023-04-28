8e5f64b777692b885883e294450f69d64ac9677c
Karen <122666632+Kar016@users.noreply.github.com>
2023-04-27 19:50:34 -0700

    Add files via upload

---
+++ C##/Programa #1/Problema #32/Problema #32/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Problema__32
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAbrir_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                // Aqui va el codigo para abrir y leer el archivo
+
+                lblAbrir.Text = openFileDialog1.FileName;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                // Aqui va el codigo para abrir y leer el archivo
+
+                lblAbrir.Text = saveFileDialog1.FileName;
+            }
+        }
+    }
+}
