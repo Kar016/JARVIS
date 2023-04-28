91bb7306bde6bc82175a2533386cc40e29390f1a
Karen <122666632+Kar016@users.noreply.github.com>
2023-04-27 19:50:04 -0700

    Add files via upload

---
+++ C##/Programa #1/Problema #31/Problema #31/Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace Problema__31
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.monthCalendar1 = new System.Windows.Forms.MonthCalendar();
+            this.lblInicio = new System.Windows.Forms.Label();
+            this.lblFinal = new System.Windows.Forms.Label();
+            this.btnFechas = new System.Windows.Forms.Button();
+            this.lblSeleccion = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // monthCalendar1
+            // 
+            this.monthCalendar1.FirstDayOfWeek = System.Windows.Forms.Day.Sunday;
+            this.monthCalendar1.Location = new System.Drawing.Point(18, 18);
+            this.monthCalendar1.MaxSelectionCount = 30;
+            this.monthCalendar1.MonthlyBoldedDates = new System.DateTime[] {
+        new System.DateTime(2023, 4, 10, 0, 0, 0, 0)};
+            this.monthCalendar1.Name = "monthCalendar1";
+            this.monthCalendar1.TabIndex = 0;
+            this.monthCalendar1.DateChanged += new System.Windows.Forms.DateRangeEventHandler(this.monthCalendar1_DateChanged);
+            // 
+            // lblInicio
+            // 
+            this.lblInicio.AutoSize = true;
+            this.lblInicio.Location = new System.Drawing.Point(30, 202);
+            this.lblInicio.Name = "lblInicio";
+            this.lblInicio.Size = new System.Drawing.Size(35, 13);
+            this.lblInicio.TabIndex = 1;
+            this.lblInicio.Text = "label1";
+            // 
+            // lblFinal
+            // 
+            this.lblFinal.AutoSize = true;
+            this.lblFinal.Location = new System.Drawing.Point(30, 243);
+            this.lblFinal.Name = "lblFinal";
+            this.lblFinal.Size = new System.Drawing.Size(35, 13);
+            this.lblFinal.TabIndex = 2;
+            this.lblFinal.Text = "label2";
+            // 
+            // btnFechas
+            // 
+            this.btnFechas.Location = new System.Drawing.Point(307, 18);
+            this.btnFechas.Name = "btnFechas";
+            this.btnFechas.Size = new System.Drawing.Size(122, 23);
+            this.btnFechas.TabIndex = 3;
+            this.btnFechas.Text = "Selecionar fechas";
+            this.btnFechas.UseVisualStyleBackColor = true;
+            this.btnFechas.Click += new System.EventHandler(this.btnFechas_Click);
+            // 
+            // lblSeleccion
+            // 
+            this.lblSeleccion.AutoSize = true;
+            this.lblSeleccion.Location = new System.Drawing.Point(30, 287);
+            this.lblSeleccion.Name = "lblSeleccion";
+            this.lblSeleccion.Size = new System.Drawing.Size(35, 13);
+            this.lblSeleccion.TabIndex = 4;
+            this.lblSeleccion.Text = "label2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(745, 549);
+            this.Controls.Add(this.lblSeleccion);
+            this.Controls.Add(this.btnFechas);
+            this.Controls.Add(this.lblFinal);
+            this.Controls.Add(this.lblInicio);
+            this.Controls.Add(this.monthCalendar1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MonthCalendar monthCalendar1;
+        private System.Windows.Forms.Label lblInicio;
+        private System.Windows.Forms.Label lblFinal;
+        private System.Windows.Forms.Button btnFechas;
+        private System.Windows.Forms.Label lblSeleccion;
+    }
+}
+
---
+++ C##/Programa #1/Problema #31/Problema #31/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Problema__31
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            monthCalendar1.BoldedDates = new DateTime[]
+            {
+                new DateTime(2023,5,15),
+                new DateTime(2023,7,10),
+                DateTime.Today.AddDays(5)
+            };
+        }
+
+        private void btnFechas_Click(object sender, EventArgs e)
+        {
+            DateTime inicio = monthCalendar1.SelectionStart; // Fecha de inicio
+            DateTime final = monthCalendar1.SelectionEnd; // Fecha final
+
+            lblInicio.Text = inicio.ToString();
+            lblFinal.Text = final.ToString();
+        }
+
+        private void monthCalendar1_DateChanged(object sender, DateRangeEventArgs e)
+        {
+            lblSeleccion.Text = monthCalendar1.SelectionRange.ToString();
+        }
+    }
+}
